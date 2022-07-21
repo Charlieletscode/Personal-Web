381547c02d6bac003f6265f4e6663f52fc83c8da
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2022-07-21 10:57:08 +0800

    Add files via upload

---
+++ Lab3/Lab3/Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Find Anagrams
+        </title>
+        <style type="text/css">
+        /*    table {
+                width: 100%;
+                border: 1px solid;
+            }
+        */    .m {
+                border-width: thin;
+                border-style: solid;
+                margin: auto;
+                width: 400px;
+                background-color: #C0C0C0;
+            }
+            .bot {
+                border-width: thin;
+                padding: 20px;
+                background-color: #FFFFFF;
+                border-top-style: solid;
+                text-align: center;
+            }
+            .top {
+                padding: 50px;
+                height: 191px;
+            }
+        </style>
+
+    </head>
+<body>
+    <form id="form1" runat="server">
+                <div style="background-color: #C0C0C0; border-style: solid; border-width: 1px; margin: auto; width: 290px; padding: 10px">
+
+            <h1 style="text-align: center">Find Anagrams</h1>
+
+            <br>
+            Enter a character string:&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="TextBox1" runat="server" Width="71px"></asp:TextBox>
+                    <br><br>
+                    <asp:CheckBox ID="CheckBox1" runat="server" Text =" Eliminate Duplicates"/>
+            <br>
+            <p style="text-align: center">
+                <asp:ListBox ID="ListBox1" runat="server" Rows="4" style="height:85px;width:150px;"></asp:ListBox>
+&nbsp;</p>
+            <br>
+            <p style="text-align: center">
+                    <asp:Button ID="Button1" runat="server" Text="Find Anagrams" OnClick="Button1_Click" />
+            </p>
+            <asp:Label runat="server" ID="co"></asp:Label>
+
+        </div>
+
+    </form>
+
+</body>
+</html>
---
+++ Lab3/Lab3/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            if (TextBox1.Text.Equals("") || TextBox1.Text.Length > 6)
+            {
+                co.Text = "Please enter a string from 1 to 6 characters";
+            }
+            else
+            {
+                anagram(TextBox1.Text, TextBox1.Text.Length, "");               {
+                    //for (int i = 0; i < ListBox1.Items.Count-1; i++)
+                    //{
+                    //    String temp = ListBox1.Items[i].ToString(); 
+                    //    for (int j = 1; j < ListBox1.Items.Count; j++)
+                    //    {
+                    //        if (ListBox1.Items[j].Text.Equals(temp))
+                    //            ListBox1.Items.RemoveAt(j);
+                    //    }
+                    //}
+                    //var arr = this.ListBox1.Items.Cast<string>().Distinct().ToArray();
+
+
+                    //ListBox1.Items.Clear();
+                    //for (int i = 0; i < ListBox1.Items.Count; i++)
+                    //{ 
+                    //    ListBox1.Items[i].Text = arr[i];
+                    //}
+                        // ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1);
+                    }
+                co.Text = ListBox1.Items.Count + " anagrams found.";
+            }
+            TextBox1.Text = "";
+        }
+        String anagram(String t, int lenght, String a)
+        {
+            if (lenght == 0)
+            {
+                if (CheckBox1.Checked && ListBox1.Items.Contains(new ListItem(a))) return "";
+                else ListBox1.Items.Add(a.ToString()); return t.ToString();
+            }
+            for (int i = 0; i < lenght; i++)
+            {
+                char c = t[i];
+
+                String lsub = t.Substring(0, i);
+                String rsub = t.Substring(i + 1);
+                String total = lsub + rsub;
+                anagram(total, total.Length, a + c);
+            }
+            return t;
+        }
+
+    }
+
+}
